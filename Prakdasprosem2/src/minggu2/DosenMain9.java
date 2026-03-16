80e72e124f46c41f7f348b70d9a5f59c7bc231cf
daffaramdhn60 <daffaramdhn60@gmail.com>
2026-03-16 13:17:56 +0700

    dosenmain9.java

--- Prakdasprosem2/src/minggu2/DosenMain9.java
+++ Prakdasprosem2/src/minggu2/DosenMain9.java
@@ -1,6 +1,7 @@
 package minggu2;
 
 public class DosenMain9 {
+    public static void main(String[] args) {
 
         
         Dosen9 dsn1 = new Dosen9();
@@ -15,5 +16,16 @@ public class DosenMain9 {
         dsn1.hitungMasaKerja = 2025;
         dsn1.ubahKeahlian = "Biologi Murni";
         dsn1.tampilkanInformasi();
+
+        Dosen9 dsn2 = new Dosen9("254334", "Pak Dedi", true, 2006, "Fisika");
+        dsn2.tampilkanInformasi();
+        dsn2.hitungMasaKerja(2025);
+        dsn2.ubahKeahlian("Kimia");
+
+        Dosen9 dsn3 = new Dosen9("254776", "PakFendi", false, 2002, "Sejarah");
+        dsn3.tampilkanInformasi();
+        dsn3.setStatusAktif(true);
+        dsn3.hitungMasaKerja(2025);
+    }
 
 } 
