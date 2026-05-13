8e8821ed79f7530a5546913fc10dcf298f871a21
daffaramdhn60 <daffaramdhn60@gmail.com>
2026-05-13 12:59:47 +0700

    Surat09

---
+++ Prakdasprosem2/bin/minggu9/Surat09.java
@@ -0,0 +1,19 @@
+public class Surat09 {
+        String idSurat;
+    String namaMahasiswa;
+    String kelas;
+    char jenisIzin;
+    int durasi;
+
+    Surat09() {
+
+    }
+
+    Surat09(String idSurat, String namaMahasiswa, String kelas, char jenisIzin, int durasi) {
+        this.idSurat = idSurat;
+        this.namaMahasiswa = namaMahasiswa;
+        this.kelas = kelas;
+        this.jenisIzin = jenisIzin;
+        this.durasi = durasi;
+    }
+}
