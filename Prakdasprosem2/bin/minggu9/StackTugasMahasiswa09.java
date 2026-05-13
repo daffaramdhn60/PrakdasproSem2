b3b27f400df29a84d74c7aca180c677a2650c310
daffaramdhn60 <daffaramdhn60@gmail.com>
2026-05-13 12:42:25 +0700

    Percobaan1

---
+++ Prakdasprosem2/bin/minggu9/StackTugasMahasiswa09.java
@@ -0,0 +1,94 @@
+package minggu9;
+
+import minggu1.Mahasiswa09;
+
+public class StackTugasMahasiswa09 {
+    Mahasiswa09 [] stack;
+    int top;
+    int size;
+
+    public StackTugasMahasiswa09(int size) {
+        this.size = size;
+        stack = new Mahasiswa09[size];
+        top = -1;
+    }
+
+    public boolean isFull() {
+        if (top == size - 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isEmpty() {
+        if (top == -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void push(Mahasiswa09 mhs) {
+        if (!isFull()) {
+            top++;
+            stack[top] = mhs;
+        } else {
+            System.out.println("Stack penuh! Tidak bisa menambahkan tugas lagi.");
+        }
+    }
+
+    public Mahasiswa09 pop() {
+        if (!isEmpty()) {
+            Mahasiswa09 m = stack[top];
+            top--;
+            return m;
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas untuk dinilai.");
+            return null;
+        }
+    }
+
+    public Mahasiswa09 peek() {
+        if (!isEmpty()) {
+            return stack[top];
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas yang dikumpulkan.");
+            return null;
+        }
+    }
+
+    public Mahasiswa09 peekBottom() {
+        if (!isEmpty()) {
+            return stack[0];
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas yang dikumpulkan.");
+            return null;
+        }
+    }
+
+    public int count() {
+        return top + 1;
+    }
+
+    public void print() {
+        for (int i = top; i >= 0; i--) {
+            System.out.println(stack[i].nama + "\t" + stack[i].nim + "\t" + stack[i].kelas);
+        }
+        System.out.println("");
+    }
+
+    public String konversiDesimalKeBiner(int nilai) {
+        StackKonversi09 stack = new StackKonversi09();
+        while (nilai != 0) {
+            int sisa = nilai % 2;
+            stack.push(sisa);
+            nilai = nilai / 2;
+        }
+        String biner = new String();
+        while (!stack.isEmpty()) {
+            biner += stack.pop();
+        }
+        return biner;
+    }
+}
