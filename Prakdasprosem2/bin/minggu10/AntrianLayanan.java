4815386384ed36402226aae3197a6d2205274de5
daffaramdhn60 <daffaramdhn60@gmail.com>
2026-05-13 13:11:38 +0700

    AntrianLayanan

---
+++ Prakdasprosem2/bin/minggu10/AntrianLayanan.java
@@ -0,0 +1,93 @@
+package Prakdasprosem2.bin.minggu10;
+
+public class AntrianLayanan {
+    Mahasiswa09[] data;
+    int front;
+    int rear;
+    int size;
+    int max;
+
+    public AntrianLayanan(int max) {
+        this.max = max;
+        this.data = new Mahasiswa09[max];
+        this.front = 0;
+        this.rear = -1;
+        this.size = 0;
+    }
+
+    public boolean isEmpty() {
+        if (size == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isFull() {
+        if (size == max) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void tambahAntrian(Mahasiswa09 mhs) {
+        if (isFull()) {
+            System.out.println("Antrian penuh, tidak dapat menambahkan mahasiswa.");
+            return;
+        }
+        rear = (rear + 1) % max;
+        data[rear] = mhs;
+        size++;
+        System.out.println(mhs.nama + " berhasil masuk ke antrian.");
+    }
+
+    public Mahasiswa09 layaniMahasiswa08() {
+        if (isEmpty()) {
+            System.out.println("Antrian kosong");
+            return null;
+        }
+        Mahasiswa09 mhs = data[front];
+        front = (front + 1) % max;
+        size--;
+        return mhs;
+    }
+
+    public void lihatTerdepan() {
+        if (isEmpty()) {
+            System.out.println("Antrian kosong.");
+        } else {
+            System.out.print("Mahasiswa terdepan : ");
+            System.out.println("NIM - NAMA - PRODI - KELAS");
+            data[front].tampilkanData();
+        }
+    }
+
+    public void lihatAkhir() {
+        if (isEmpty()) {
+            System.out.println("Antrian kosong.");
+        } else {
+            System.out.print("Mahasiswa paling belakang : ");
+            System.out.println("NIM - NAMA - PRODI - KELAS");
+            data[rear].tampilkanData();
+        }
+    }
+
+    public void tampilkanSemua() {
+        if (isEmpty()) {
+            System.out.println("Antrian Kosong");
+            return;
+        }
+        System.out.println("Daftar Mahasiswa dalam Antrian : ");
+        System.out.println("NIM - NAMA - PRODI - KELAS");
+        for (int i = 0; i < size; i++) {
+            int index = (front + i) % max;
+            System.out.print((i + 1) + ". ");
+            data[index].tampilkanData();
+        }
+    }
+
+    public int getJumlahAntrian() {
+        return size;
+    }
+}
